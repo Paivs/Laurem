0d6240440d3b42fcbe3f7fa52cdff98c48083515
willreis <william.reis.silva@gmail.com>
2025-07-25 10:07:12 -0300

    Ajustes blog merge

--- frontend/src/app/(auth)/blog/page.jsx
+++ frontend/src/app/(auth)/blog/page.jsx
@@ -1,3 +1,4 @@
+
 import Articles from "@/components/Articles";
 import { apiServer } from "@/lib/api-server.server";
 import HeroBlog from "@/components/blocks/hero-blog";
@@ -13,20 +14,16 @@ export default async function BlogSection({ searchParams }) {
   // Busca os artigos
   const articles = await apiServer.public.get(`articles?page=${currentPage}`);
   console.log(articles);
+  
 
   return (
-    <section className="py-12 md:py-20 lg:py-24">
+    <section className="pt-14 md:pt-0">
+     <HeroBlog />
+    
       <div className="container mx-auto space-y-6 px-4 md:space-y-8 md:px-6 2xl:max-w-[1400px]">
-        {/* Título Principal Aprimorado */}
-        <div className="text-center mb-12 space-y-4">
-          <h1 className="text-4xl font-extrabold py-2 tracking-tight md:text-5xl lg:text-6xl text-primary ">
-            Tecnologia e outros
-          </h1>
-          <p className="text-lg text-muted-foreground max-w-2xl mx-auto md:text-xl">
-            Explore perspectivas disruptivas do cenário digital
-          </p>
-        </div>
-
+        <h1 className="text-4xl font-extrabold text-center mb-8 md:text-5xl">
+          Tech Insights & Trends
+        </h1>
         <Articles initialArticles={articles} currentPage={currentPage} />
       </div>
     </section>
